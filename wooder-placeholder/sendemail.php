dce4f5c0da71a8f2de6afab0848a7274e5339e45
mnn jhn <michaelmontero9319@gmail.com>
2023-11-18 19:54:43 -0500

    Modify or delete files as needed
    presenting work done so far, further edits to be discussed

--- wooder-placeholder/sendemail.php
+++ wooder-placeholder/sendemail.php
@@ -8,7 +8,7 @@
 	
 	//Headers
 	// Add Recipient Email Here.
-	$to = "alihamza292@gmail.com";
+	$to = "mike.montero1993@gmail.com";
     $subject = 'Contact Us';
 	$headers = "MIME-Version: 1.0\r\n";
 	$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
